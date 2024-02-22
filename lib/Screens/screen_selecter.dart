663abcbf7d6002801b1782710890aecc84091525
muzzammil763 <deadbase763@gmail.com>
2024-02-22 21:53:32 +0500

    added a button on screen selecter screen for navigating

--- lib/Screens/screen_selecter.dart
+++ lib/Screens/screen_selecter.dart
@@ -5,6 +5,52 @@ class ScreenSelecter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xff22343C),
+        title: const Text(
+          "Select Screen",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 10,
+            width: double.infinity,
+            color: const Color(0xff22343C),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          Column(
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: Container(
+                  height: 60,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: const Color(0xff22343C),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "Feed 1",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
